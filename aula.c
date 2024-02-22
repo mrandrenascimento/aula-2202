56f762ee35ec30ee76f3aa4e881434e7d8cfa942
mrandrenascimento <159596002+mrandrenascimento@users.noreply.github.com>
2024-02-22 11:29:24 -0300

    Add files via upload

---
+++ aula.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	int idade = 10; //inteiro 
+	float peso = 80.550;//real
+	char sexo = 'f';//caracter=apenas uma letra ou caracter
+	char nome[200] = "marta";//cadeia= mais de uma letra ou caracter
+	
+	printf("idade: %d \n",idade);//i=int
+	printf("peso: %f \n",peso);//f=float
+	printf("sexo: %c \n",sexo);//c= chat
+	printf("peso: %s \n",peso);//s= string (cedeia)
+	
+	return 0;
+}
