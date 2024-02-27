316c711016d54ef7777debd97c3d1c67df0298f7
mrandrenascimento <159596002+mrandrenascimento@users.noreply.github.com>
2024-02-27 10:30:52 -0300

    Add files via upload

---
+++ Sem Título6.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+	float primeironumero, segundonumero;
+	float soma, subtracao, multiplicacao,divisao, media;
+	
+	printf("Digite o primeiro numero: ");
+	scanf("%f",&primeironumero);
+	printf("Dogite o Segundo numero: ");
+	scanf("%if",&segundonumero);
+	
+	soma=primeironumero+segundonumero
+	subtracao=primeironumero-segundonumero
+	divisao=primeironumero/segundonumero
+	multiplicacao=primeironumero/segundonumero
+	
+	media=(rimeironumero+segundonumero)/2
+	
+	system(cls)
+	
+	printf("primeiro numero: %f \n",primeironumero);
+	printf("Segundo Numero: %f \n", segundonumero);
+	printf("Soma: %f \n",soma);
+	primtf()
