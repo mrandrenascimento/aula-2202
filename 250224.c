941290f6326b4d7f48798114cd3f61a5aaa65f4a
mrandrenascimento <159596002+mrandrenascimento@users.noreply.github.com>
2024-02-28 11:10:12 -0300

    Add files via upload

--- 250224.c
+++ 250224.c
@@ -2,13 +2,17 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h> //Biblioteca
 void cabecalho(){
+	
 
 	system("cls\\clear");
 	printf("\n=== SENAI DENDEZEIRO ===\n");
+	fflush(stdin);
 }
+
 int main(){
-	
+	setlocale(LC_ALL, "");
 	char nome[200];
 	int idade;
 	float primeiranota, segundanota, terceiranota, media;
@@ -22,18 +26,26 @@ int main(){
 	cabecalho();
 	printf("Digite a Primeira Nota: ");
 	scanf("%f",&primeiranota);
+
+	cabecalho();
 	printf("Digite a Segunda Nota: ");
 	scanf("%f",&segundanota);
-	printf("Digite a terceira Nota: ");
-	scanf("%f",&terceiranota);
+
+	cabecalho();
+	printf("Digite a terceira Nota: ");// na tecla crlt mais espaça para nao ter erros
+	scanf("%.2f",&terceiranota);
+	
+	media=(primeiranota+segundanota+terceiranota)/3;
 	
 	cabecalho();
+	
+	printf("\n Média: %.2f",media);
 
-	media=(primeiranota+segundanota+terceiranota)/3;
-	if("%.2f",media>=7){
-	printf("%f\nAPROVADO");}
-	else{ printf("%f\nREPROVADO");
+	if(media>=7){
+	printf("\nAPROVADO");}
+	else{ printf("\nREPROVADO");
 	}
+	
 	
 	
 	
