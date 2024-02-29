85d4b15e77aafbf0933133e2f62d05bf210a10cd
mrandrenascimento <159596002+mrandrenascimento@users.noreply.github.com>
2024-02-29 10:38:11 -0300

    Add files via upload

---
+++ Algaritmo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h> //Bibliotecas
+#include<string.h>
+void cabecalho(){
+	
+	setlocale(LC_ALL, "");
+	system("cls\\clear");
+	printf("\n=== SENAI DENDEZEIRO ===\n");
+	fflush(stdin);
+}
+int main(){
+	int j;
+	for(j=10; j>=1; j--){
+		printf("%d \n",j);
+	}return 0;
+}
