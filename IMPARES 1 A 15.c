824969dae01130d24ac71ed930692ef6555b99d3
mrandrenascimento <159596002+mrandrenascimento@users.noreply.github.com>
2024-03-05 08:36:20 -0300

    Add files via upload

---
+++ IMPARES 1 A 15.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h> //Bibliotecas
+#include<string.h>
+void cabecalho(){
+	
+	setlocale(LC_ALL, "");
+	system("cls\\clear");
+	printf("\n=== SENAI DENDEZEIRO ===\n");
+	fflush(stdin);
+}
+int main(){
+	int i;
+	cabecalho();
+	printf("NÚMEROS IMPAR ENTRE 1 E 15");
+
+	for (i=1;i<=15;i++){
+		if(i %2!=0){
+			printf("\n%d",i);
+		
+}}
+return 0;		
+}
