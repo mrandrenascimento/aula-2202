facb46fd10edb519836b13a5bfa9725ba76ebfe6
mrandrenascimento <159596002+mrandrenascimento@users.noreply.github.com>
2024-03-07 10:06:30 -0300

    Add files via upload

---
+++ bdsmka.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h> //Bibliotecas
+#include<string.h>
+void cabecalho(){
+	
+	setlocale(LC_ALL, "");
+	system("cls\\clear");
+	printf("\n=== SENAI DENDEZEIRO ===\n");
+	fflush(stdin);
+}
+int calmcularsoma(float n1, float n2){
+
+float soma;
+soma=n1+n2;
+return soma;
+}
+
+int calmculardivisao(float n1, float n2){
+float divisao
+divisao=n1/n2;
+return divisao;
+}
+int calmcularnsubtracao(float n1, float n2){
+
+float subtracao;
+subtracao=n1-n2;
+return subtracao;
+}
+
+int calmcularmultiplicacao(float n1, float n2){
+float multiplicacao;
+subtracao=n1*n2;
+return multiplicacao;
+}
+int main(){
+	
+	
+	return 0;
+}
+
