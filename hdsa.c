639f54d03710500edca823af0f106f1832a81bf9
mrandrenascimento <159596002+mrandrenascimento@users.noreply.github.com>
2024-03-07 10:05:45 -0300

    Add files via upload

---
+++ hdsa.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h> //Bibliotecas
+#include <string.h>
+void cabecalho(){
+	
+	setlocale(LC_ALL, "");
+	system("cls\\clear");
+	printf("\n=== SENAI DENDEZEIRO ===\n");
+	fflush(stdin);
+}
+void verificarParouimpar(int numero){
+	if(numero%2==0) printf("Par");
+	else
+	printf("impar");
+}
+int main(){
+	int numero;
+	printf("Digite um Número: ");
+	scanf("%d",&numero);
+	
+	printf("\nVERIFICAR PAR OU MIMPAR\n ");
+	verificarParouimpar(numero);
+	return 0;
+}
